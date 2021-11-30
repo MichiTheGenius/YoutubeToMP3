06d05eedb2819a11da5e6adc02e8a797cf35928a
MichiTheGenius <litzinger04@gmail.com>
2021-11-30 19:44:35 +0100

    changed the download folder so it works with relative paths instead of absolute ones. Added comments for better understanding of the code

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,47 +1,53 @@
 import pytube
 import os
 
+black = "\u001b[30m"
+red = "\u001b[31m"
+green = "\u001b[32m"
+yellow = "\u001b[33m"
+blue = "\u001b[34m"
+magenta = "\u001b[35m"
+cyan = "\u001b[36m"
+white = "\u001b[37m"
+reset = "\u001b[0m"
 
-black = '\u001b[30m'
-red = '\u001b[31m'
-green = '\u001b[32m'
-yellow = '\u001b[33m'
-blue = '\u001b[34m'
-magenta = '\u001b[35m'
-cyan = '\u001b[36m'
-white = '\u001b[37m'
-reset = '\u001b[0m'
+
+def reset_color():
+    print(reset)
 
 
-def printBlueText(text):
+def print_blue_text(text):
     print(f'{blue}{text}')
+    reset_color()
 
 
-url = input('Enter url: ')
+url = input("Enter url: ")
 my_video = pytube.YouTube(url)
-# videos = []
-# video_resolutions = []
-# for stream in my_video.streams.filter(audio_codec="mp4a.40.2", mime_type='audio/mp4'):
-#     print(stream)
-#     videos.append(stream)
-#     video_resolutions.append(stream.resolution)
 
-# i = 0
-# for resolution in video_resolutions:
-#     print(f'{i}. {resolution}')
-#     i += 1
-
-#choice = int(input('Enter desired resolution number: '))
+# get the audio file out of all the options
 finished_video = my_video.streams.filter(
-    audio_codec="mp4a.40.2", mime_type='audio/mp4').first()  # videos[choice]
-printBlueText('Video is downloading!')
-finished_video.download('/home/michael/Downloads/Music')
-printBlueText('finished downloading!')
+    audio_codec="mp4a.40.2", mime_type="audio/mp4").first()
 
-printBlueText('Converting to mp3')
-mp4_file = finished_video.default_filename
-mp3_output = finished_video.default_filename.replace('.mp4', '.mp3')
-convert_command = f'ffmpeg -i "{mp4_file}" -f mp3 -ab 192000 -vn "{mp3_output}"'
+print_blue_text("Video is downloading!")
+# download that file
+finished_video.download("Music/")
+print_blue_text("finished downloading!")
+
+print_blue_text("Converting to mp3")
+
+# input filename for ffmpeg
+mp4_file = f"Music/{finished_video.default_filename}"
+
+# output mp3 file (replace the default .mp4 in the filename with .mp3)
+mp3_output = f"Music/{finished_video.default_filename.replace('.mp4', '.mp3')}"
+
+# command to convert mp4 to mp3 with ffmpeg. -i for input, -f for filetype, -ab for bitrate, -vn for no video
+convert_command = f"ffmpeg -i '{mp4_file}' -f mp3 -ab 192000 -vn '{mp3_output}'"
+
+# execute the convert command
 os.system(convert_command)
+
+# delete the remaining mp4 file we don't need anymore
 os.remove(mp4_file)
-printBlueText('finished converting!')
+
+print_blue_text("finished converting!")
