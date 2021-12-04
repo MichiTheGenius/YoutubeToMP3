6e2cf607f948bd2fb5f1d1c95378e52ec072739d
MichiTheGenius <litzinger04@gmail.com>
2021-12-04 16:15:43 +0100

    added a new script with playlist support using pytubes playlist funtionality to get all urls from playlist and remaining the self written funtionality from before to download everything

--- yt_playlist_mp3.py
+++ yt_playlist_mp3.py
@@ -1,5 +1,6 @@
 import pytube
 import os
+from pytube import Playlist
 
 black = "\u001b[30m"
 red = "\u001b[31m"
@@ -11,15 +12,28 @@ cyan = "\u001b[36m"
 white = "\u001b[37m"
 reset = "\u001b[0m"
 
+def get_playlist_urls(url):
+    playlist = Playlist(url)
+
+    video_urls = playlist.video_urls
+
+    return video_urls
+
 def reset_color():
     print(reset)
+
 
 def print_blue_text(text):
     print(f'{blue}{text}', end="")
     reset_color()
 
+def print_red_text(text):
+    print(f'{red}{text}', end="")
+    reset_color()
+
 def filter_out_correct_video(video):
     return video.streams.filter(audio_codec="mp4a.40.2", mime_type="audio/mp4").first()
+
 
 def get_mp4_file(path, correct_video):
     if not path.endswith("/"):
@@ -27,15 +41,18 @@ def get_mp4_file(path, correct_video):
     else:
         return f"{path}{correct_video.default_filename}"
 
+
 def get_path_from_file():
     path = ""
     with open("path.txt", "r") as path_file:
         path = path_file.read()
     return path
 
+
 def write_path_to_file(path):
     with open("path.txt", "w") as path_file:
         path_file.write(path)
+
 
 def change_path():
     current_path = get_path_from_file()
@@ -43,23 +60,33 @@ def change_path():
     if new_path != "":
         write_path_to_file(new_path)
 
+
 def list_path():
     current_path = get_path_from_file()
     print(f"The current path is {current_path}")
 
+
 def main():
     while True:
-        url = input("Enter url(or q to quit, c to change the path, l to list the path): ")
-        if url.lower() == 'q':
-            break
-        elif url.lower() == 'c':
+        playlist_url_input = input("Enter playlist url(or q to quit, c to change the path, l to list the path): ")
+        if playlist_url_input.lower() == 'q':
+            quit()
+        elif playlist_url_input.lower() == 'c':
             change_path()
-            continue
-        elif url.lower() == 'l':
+            continue 
+        elif playlist_url_input.lower() == 'l':
             list_path()
             continue
+        else:
+            break
 
-        pytube_video = pytube.YouTube(url)
+    playlist_urls = get_playlist_urls(playlist_url_input)
+
+    start_index = int(input("Enter start index: ")) - 1
+    end_index = int(input("Enter end index: ")) - 1
+
+    for i in range(start_index, end_index+1):
+        pytube_video = pytube.YouTube(playlist_urls[i])
 
         download_path = get_path_from_file()
         print_blue_text(f"video is downloading to {download_path}!")
@@ -89,7 +116,8 @@ def main():
         os.remove(mp4_file)
 
         print_blue_text("finished converting!")
-        print_blue_text(f"Find your tunes in the folder {download_path}!")
+    print_red_text(f"Finished downloading all of your videos! Find your tunes in the folder {download_path}!")
+
 
 if __name__ == "__main__":
     main()
