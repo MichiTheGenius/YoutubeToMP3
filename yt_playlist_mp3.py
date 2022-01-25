1c92f37f1925b5fbda13a27eb2fd211e412318a6
MichiTheGenius <litzinger04@gmail.com>
2022-01-25 14:00:45 +0100

    converted the playlist script to OOP

--- yt_playlist_mp3.py
+++ yt_playlist_mp3.py
@@ -34,25 +34,25 @@ class Converter():
         print(f'{blue}{text}', end="")
         self.reset_color()
 
-    def print_yellow_text(text):
+    def print_yellow_text(self, text):
         print(f'{yellow}{text}', end="")
-        reset_color()
+        self.reset_color()
 
-    def print_yellow_text(text):
+    def print_yellow_text(self, text):
         print(f'{yellow}{text}', end="")
-        reset_color()
+        self.reset_color()
 
-    def print_red_text(text):
+    def print_red_text(self, text):
         print(f'{red}{text}', end="")
-        reset_color()
+        self.reset_color()
 
 
-    def filter_out_correct_video(video):
+    def filter_out_correct_video(self, video):
         # get correct video format -> this took a lot of trying around to fine tune
         return video.streams.filter(audio_codec="mp4a.40.2", mime_type="audio/mp4").first()
 
 
-    def get_mp4_file(path, correct_video):
+    def get_mp4_file(self, path, correct_video):
         # replace the single and double quotes with nothing from the song name as it causes errors
         default_filename = correct_video.default_filename.replace("'", "").replace('"', '')
         # if the path from the user doesn't have a "/" at the end we need to add it
@@ -62,7 +62,7 @@ class Converter():
             return f"{path}{default_filename}"
 
 
-    def get_mp3_file(path, correct_video, channel_name):
+    def get_mp3_file(self, path, correct_video, channel_name):
         # replace the single and double quotes with nothing from the song name as it causes errors
         default_filename = correct_video.default_filename.replace(".mp4", ".mp3").replace("'", "").replace('"', '')
         channel_name = channel_name.replace("'", "").replace('"', '')
@@ -73,7 +73,7 @@ class Converter():
             return f"{path}{channel_name} - {default_filename}"
 
 
-    def convert_mp4_to_mp3(mp4_file, mp3_file):
+    def convert_mp4_to_mp3(self, mp4_file, mp3_file):
         # command to convert mp4 to mp3 with ffmpeg. -i for input, -f for filetype, -ab for bitrate, -vn for no video
         convert_command = f"ffmpeg -i '{mp4_file}' -f mp3 -ab 192000 -vn '{mp3_file}'"
 
@@ -81,7 +81,7 @@ class Converter():
         os.system(convert_command)
 
 
-    def get_path_from_file():
+    def get_path_from_file(self):
         # open the path file and assign a path variable the content of that file
         path = ""
         with open("path.txt", "r") as path_file:
@@ -89,51 +89,51 @@ class Converter():
         return path
 
 
-    def path_file_exists():
+    def path_file_exists(self):
         return os.path.isfile("path.txt")
 
 
-    def write_path_to_file(path):
+    def write_path_to_file(self, path):
         with open("path.txt", "w") as path_file:
             path_file.write(path)
 
 
-    def change_path():
+    def change_path(self):
         new_path = ""
-        if path_file_exists():
-            current_path = get_path_from_file()
+        if sefl.path_file_exists():
+            current_path = self.get_path_from_file()
             new_path = input(f"Enter a new download path (The current one is {current_path}): ")
         else:
             new_path = input(f"Enter a new download path: ")
 
         # if the inputted path is empty we don't write it to the file
         if new_path != "":
-            write_path_to_file(new_path)
+           self.write_path_to_file(new_path)
 
 
-    def list_path():
-        if path_file_exists():
-            current_path = get_path_from_file()
+    def list_path(self):
+        if self.path_file_exists():
+            current_path = self.get_path_from_file()
             print(f"The current path is {current_path}")
         else:
             print("You don't have a path file yet! Create one by entering c in the url field.")
 
 
-    def download_playlist():
+    def download_playlist(self):
         while True:
             playlist_url_input = input("Enter playlist url(or q to quit, c to change the path, l to list the path): ")
             if playlist_url_input.lower() == 'q':
                 quit()
             elif playlist_url_input.lower() == 'c':
-                change_path()
+                self.change_path()
                 continue  # jump to start of while loop -> ask the user again what he wants to do
             elif playlist_url_input.lower() == 'l':
-                list_path()
+                self.list_path()
                 continue
             else:
                 break
 
-        playlist_urls = get_playlist_urls(playlist_url_input)
+        playlist_urls = self.get_playlist_urls(playlist_url_input)
 
         amount_of_videos = len(playlist_urls)
         print(f"You have {amount_of_videos} videos in your playlist")
@@ -150,35 +150,35 @@ class Converter():
             channel_name = pytube.Channel(channel_url).channel_name
 
             # get the current download path from the text file
-            download_path = get_path_from_file()
-            print_blue_text(f"video is downloading to {download_path}!")
+            download_path = self.get_path_from_file()
+            self.print_blue_text(f"video is downloading to {download_path}!")
             
             # i + 1 because the loop starts at 0 but youtube starts at 1
-            print_yellow_text(f"You are currently at video {i+1} of {end_index}")
+            self.print_yellow_text(f"You are currently at video {i+1} of {end_index}")
 
             # get the audio file out of all the options
-            correct_video = filter_out_correct_video(pytube_video)
+            correct_video = self.filter_out_correct_video(pytube_video)
 
             # download that file
             correct_video.download(download_path)
-            print_blue_text("finished downloading!")
+            self.print_blue_text("finished downloading!")
 
-            print_blue_text("converting to mp3!")
+            self.print_blue_text("converting to mp3!")
 
             # input filename for ffmpeg
-            mp4_file = get_mp4_file(download_path, correct_video)
+            mp4_file = self.get_mp4_file(download_path, correct_video)
             # output mp3 file (replace the default .mp4 in the filename with .mp3)
-            mp3_file = get_mp3_file(download_path, correct_video, channel_name)
+            mp3_file = self.get_mp3_file(download_path, correct_video, channel_name)
             
-            convert_mp4_to_mp3(mp4_file,mp3_file)
+            self.convert_mp4_to_mp3(mp4_file,mp3_file)
             # delete the remaining mp4 file we don't need anymore
             os.remove(mp4_file)
 
-            print_blue_text("finished converting!")
-        print_red_text(f"Finished downloading all of your videos! Find your tunes in the folder {download_path}!")
+            self.print_blue_text("finished converting!")
+        self.print_red_text(f"Finished downloading all of your videos! Find your tunes in the folder {download_path}!")
 
 
 
 
 if __name__ == "__main__":
-    main()
+   converter = Converter() 
