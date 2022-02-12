6217b8c1c9d2004bfced1c41e5a37baf86af15da
MichiTheGenius <litzinger04@gmail.com>
2022-02-12 13:21:28 +0100

    moved functions that get constantly reused to utils.py
    they now get used in the single vid script

---
+++ utils.py
@@ -0,0 +1,68 @@
+import os
+def get_path_from_file():
+    # open the path file and assign a path variable the content of that file
+    path = ""
+    with open("path.txt", "r") as path_file:
+        path = path_file.read()
+    return path
+
+def path_file_exists():
+    return os.path.isfile("path.txt")
+
+def write_path_to_file(path):
+    with open("path.txt", "w") as path_file:
+        path_file.write(path)
+
+def change_path():
+    new_path = ""
+    if path_file_exists():
+        current_path = get_path_from_file()
+        new_path = input(
+            f"Enter a new download path (The current one is {current_path}): ")
+    else:
+        new_path = input(f"Enter a new download path: ")
+
+    # if the inputted path is empty we don't write it to the file
+    if new_path != "":
+        write_path_to_file(new_path)
+
+def list_path():
+    if path_file_exists():
+        current_path = get_path_from_file()
+        print(f"The current path is {current_path}")
+    else:
+        print(
+            "You don't have a path file yet! Create one by entering c in the url field.")
+
+def filter_out_correct_video(video):
+    # get correct video format -> this took a lot of trying around to fine tune
+    return video.streams.filter(audio_codec="mp4a.40.2", mime_type="audio/mp4").first()
+
+def get_mp4_file(path, correct_video):
+    # replace the single and double quotes with nothing from the song name as it causes errors
+    default_filename = correct_video.default_filename.replace(
+        "'", "").replace('"', '')
+
+    # if the path from the user doesn't have a "/" at the end we need to add it
+    if not path.endswith("/"):
+        return f"{path}/{default_filename}"
+    else:
+        return f"{path}{default_filename}"
+
+def get_mp3_file(path, correct_video, channel_name):
+    # replace the single and double quotes with nothing from the song name as it causes errors
+    default_filename = correct_video.default_filename.replace(
+        ".mp4", ".mp3").replace("'", "").replace('"', '')
+    channel_name = channel_name.replace("'", "").replace('"', '')
+    # if the path from the user doesn't have a "/" at the end we need to add it
+    if not path.endswith("/"):
+        return f"{path}/{channel_name} - {default_filename}"
+    else:
+        return f"{path}{channel_name} - {default_filename}"
+
+def convert_mp4_to_mp3(mp4_file, mp3_file):
+    # command to convert mp4 to mp3 with ffmpeg. -i for input, -f for filetype, -ab for bitrate, -vn for no video
+    convert_command = f"ffmpeg -i '{mp4_file}' -f mp3 -ab 192000 -vn '{mp3_file}'"
+
+    # execute the convert command
+    os.system(convert_command)
--- yt_mp3.py
+++ yt_mp3.py
@@ -1,11 +1,11 @@
 import pytube
 import os
 import colors
-
+import utils
 
 class Converter():
     def __init__(self):
-        if not self.path_file_exists():
+        if not utils.path_file_exists():
             print(
                 "You don't have a path file yet! Create one by entering c in the url field.")
         self.download_video()
@@ -17,10 +17,10 @@ class Converter():
             if url.lower() == 'q':
                 break  # quit the loop -> program finishes
             elif url.lower() == 'c':
-                self.change_path()
+                utils.change_path()
                 continue  # jump to start of while loop -> ask the user again what he wants to do
             elif url.lower() == 'l':
-                self.list_path()
+                utils.list_path()
                 continue
 
             # make a video out of the current url in the loop that pytube can use
@@ -28,11 +28,11 @@ class Converter():
             channel_url = pytube_video.channel_url
             channel_name = pytube.Channel(channel_url).channel_name
 
-            download_path = self.get_path_from_file()
+            download_path = utils.get_path_from_file()
             colors.print_blue_text(f"video is downloading to {download_path}!")
 
             # get the audio file out of all the options
-            correct_video = self.filter_out_correct_video(pytube_video)
+            correct_video = utils.filter_out_correct_video(pytube_video)
 
             # download that file
             correct_video.download(download_path)
@@ -40,87 +40,18 @@ class Converter():
             colors.print_blue_text("converting to mp3!")
 
             # input filename for ffmpeg
-            mp4_file = self.get_mp4_file(download_path, correct_video)
+            mp4_file = utils.get_mp4_file(download_path, correct_video)
 
             # output mp3 file (replace the default .mp4 in the filename with .mp3)
-            mp3_file = self.get_mp3_file(
+            mp3_file = utils.get_mp3_file(
                 download_path, correct_video, channel_name)
 
-            self.convert_mp4_to_mp3(mp4_file, mp3_file)
+            utils.convert_mp4_to_mp3(mp4_file, mp3_file)
 
             # delete the remaining mp4 file we don't need anymore
             os.remove(mp4_file)
 
             colors.print_blue_text("finished converting!")
-
-    def filter_out_correct_video(self, video):
-        # get correct video format -> this took a lot of trying around to fine tune
-        return video.streams.filter(audio_codec="mp4a.40.2", mime_type="audio/mp4").first()
-
-    def get_mp4_file(self, path, correct_video):
-        # replace the single and double quotes with nothing from the song name as it causes errors
-        default_filename = correct_video.default_filename.replace(
-            "'", "").replace('"', '')
-
-        # if the path from the user doesn't have a "/" at the end we need to add it
-        if not path.endswith("/"):
-            return f"{path}/{default_filename}"
-        else:
-            return f"{path}{default_filename}"
-
-    def get_mp3_file(self, path, correct_video, channel_name):
-        # replace the single and double quotes with nothing from the song name as it causes errors
-        default_filename = correct_video.default_filename.replace(
-            ".mp4", ".mp3").replace("'", "").replace('"', '')
-        channel_name = channel_name.replace("'", "").replace('"', '')
-        # if the path from the user doesn't have a "/" at the end we need to add it
-        if not path.endswith("/"):
-            return f"{path}/{channel_name} - {default_filename}"
-        else:
-            return f"{path}{channel_name} - {default_filename}"
-
-    def convert_mp4_to_mp3(self, mp4_file, mp3_file):
-        # command to convert mp4 to mp3 with ffmpeg. -i for input, -f for filetype, -ab for bitrate, -vn for no video
-        convert_command = f"ffmpeg -i '{mp4_file}' -f mp3 -ab 192000 -vn '{mp3_file}'"
-
-        # execute the convert command
-        os.system(convert_command)
-
-    def get_path_from_file(self):
-        # open the path file and assign a path variable the content of that file
-        path = ""
-        with open("path.txt", "r") as path_file:
-            path = path_file.read()
-        return path
-
-    def path_file_exists(self):
-        return os.path.isfile("path.txt")
-
-    def write_path_to_file(self, path):
-        with open("path.txt", "w") as path_file:
-            path_file.write(path)
-
-    def change_path(self):
-        new_path = ""
-        if self.path_file_exists():
-            current_path = self.get_path_from_file()
-            new_path = input(
-                f"Enter a new download path (The current one is {current_path}): ")
-        else:
-            new_path = input(f"Enter a new download path: ")
-
-        # if the inputted path is empty we don't write it to the file
-        if new_path != "":
-            self.write_path_to_file(new_path)
-
-    def list_path(self):
-        if self.path_file_exists():
-            current_path = self.get_path_from_file()
-            print(f"The current path is {current_path}")
-        else:
-            print(
-                "You don't have a path file yet! Create one by entering c in the url field.")
-
 
 if __name__ == '__main__':
     converter = Converter()
