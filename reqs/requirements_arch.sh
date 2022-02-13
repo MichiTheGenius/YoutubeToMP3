61335a6e437992742582ec95d00eafb0c44fcc98
MichiTheGenius <litzinger04@gmail.com>
2022-02-13 17:06:20 +0100

    removed the standard python version from requirements
    python3 is used by the script anyway
    no one uses original python anymore

--- reqs/requirements_arch.sh
+++ reqs/requirements_arch.sh
@@ -2,5 +2,5 @@
 
 set -xe
 
-sudo pacman -S python python3 python3-pip ffmpeg
+sudo pacman -S python3 python3-pip ffmpeg
 pip3 install pytube numpy
--- reqs/requirements_deb.sh
+++ reqs/requirements_deb.sh
@@ -3,5 +3,5 @@
 set -xe
 
 sudo apt update
-sudo apt install python python3 python3-pip ffmpeg
+sudo apt install python3 python3-pip ffmpeg
 pip3 install pytube numpy
