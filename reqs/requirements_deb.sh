cc144e558eef8c36372902eaab6b040b752682fd
MichiTheGenius <litzinger04@gmail.com>
2022-02-13 17:18:14 +0100

    pytube is now installed from the fork in requirements
    the fork from baxterisme works and the original version does not

--- reqs/requirements_deb.sh
+++ reqs/requirements_deb.sh
@@ -4,4 +4,6 @@ set -xe
 
 sudo apt update
 sudo apt install python3 python3-pip ffmpeg
-pip3 install pytube numpy
+#pip3 install pytube numpy
+pip3 install numpy
+pip3 install git+https://github.com/baxterisme/pytube
